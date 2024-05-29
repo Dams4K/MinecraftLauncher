ba8b3e45496aa3c7dedb8a3eb625493898f1e983
Dams4K <dams4k@users.noreply.github.com>
2024-05-29 14:07:46 +0200

    player is rotating

---
+++ demo/3d/player_godot.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+@onready var anchor: Node3D = $Anchor
+@onready var camera_animations: AnimationPlayer = $CameraAnimations
+
+@export var rotation_speed: float = 0.002
+@export var can_animate: bool = true
+
+var velocity_y: float = 0.0
+
+func set_camera_rotation(angle: float):
+	anchor.rotation.y = angle
+
+func get_camera_rotation():
+	return anchor.rotation.y
+
+func _process(delta: float) -> void:
+	if can_animate:
+		anchor.rotation.y += rotation_speed
+
+func launch(velocity: float):
+	pass
---
+++ demo/menus/player_viewport_container.gd
@@ -0,0 +1,21 @@
+extends SubViewportContainer
+
+@onready var player: Node3D = $SubViewport/player
+
+var start_pos: Vector2
+var start_rotation: float = 0.0
+var should_rotate: bool = false
+
+@export var rotation_force: float = 0.05
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			start_rotation = player.get_camera_rotation()
+			player.set_camera_rotation(start_rotation)
+			start_pos = event.position
+		
+		should_rotate = event.pressed
+		player.can_animate = not event.pressed
+	if event is InputEventMouseMotion and should_rotate:
+		player.set_camera_rotation(start_rotation - (event.position.x - start_pos.x) * rotation_force)
