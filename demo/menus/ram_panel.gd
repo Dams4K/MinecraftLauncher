0af8030c2ea1d5771374f39eac46800646d8d6fa
Dams4K <dams4k@users.noreply.github.com>
2024-06-23 22:48:20 +0200

    looking better

--- demo/menus/ram_panel.gd
+++ demo/menus/ram_panel.gd
@@ -3,5 +3,9 @@ extends PanelContainer
 @onready var ram_slider: VSlider = $RamSlider
 @onready var rotating_label: RotatingLabel = %RotatingLabel
 
+func _ready() -> void:
+	ram_slider.value = Config.max_ram
+
 func _process(delta: float) -> void:
 	rotating_label.args[0] = ram_slider.value
+	Config.max_ram = ram_slider.value
